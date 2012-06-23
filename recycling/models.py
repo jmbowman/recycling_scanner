40331cdab34f447525b75cb52f96cd8dc951fbf5
Jeremy Bowman <jmbowman@alum.mit.edu>
2012-06-23 15:53:06 -0400

    Dependencies and models

---
+++ recycling/models.py
@@ -0,0 +1,56 @@
+"""
+Identify, Reuse, Recycle database models
+"""
+from django.db import models
+
+
+class Address(models.Model):
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    zip_code = models.CharField(max_length=10, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.zip_code
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Maker(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(verbose_name='URL', blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=4000, blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True)
+    code = models.PositiveIntegerField(null=True, blank=True)
+    maker = models.ForeignKey(null=True, blank=True)
+    model = models.CharField(max_length=200, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Taker(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.ForeignKey(Address)
+    url = models.URLField(verbose_name='URL', blank=True)
+    email = models.EmailField(blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
+    products = models.ManyToManyField(Product, blank=True)
+    
+    def __unicode__(self):
+        return self.name
