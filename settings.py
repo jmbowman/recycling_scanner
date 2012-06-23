4457f75188775d263ad96d8f5dc83db8618bcf26
Brian R Dant <brian.r.dant@gmail.com>
2012-06-23 16:41:01 -0400

    modified:   settings.py

--- settings.py
+++ settings.py
@@ -92,7 +92,8 @@ TEMPLATE_LOADERS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.request',
-    'django.core.context_processors.static',
+	'django.contrib.auth.context_processors.auth',  
+	'django.core.context_processors.static',
 )
 
 MIDDLEWARE_CLASSES = (
