4f72ea8fbe5220c54be4aef30584b32a70375815
Brian R Dant <brian.r.dant@gmail.com>
2012-06-23 22:55:44 -0400

    Added fixtures and admin.py

---
+++ recycling/admin.py
@@ -0,0 +1,11 @@
+from models import Address, Category, Maker, Product, Taker 
+from django.contrib import admin
+
+admin.site.register(Taker)
+admin.site.register(Category)
+admin.site.register(Address)
+admin.site.register(Maker)
+admin.site.register(Product)
+
+
+
